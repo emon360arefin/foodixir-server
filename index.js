0c5acc056d99d2dc0cf1f52a1986990f2d34c87e
Emon Arefin <emon360degree@gmail.com>
2023-08-12 02:15:17 +0600

    Chef Single Page Server Revamp

--- index.js
+++ index.js
@@ -1,26 +1,175 @@
 const express = require('express')
-const app = express();
-var cors = require('cors')
-const port = 5000;
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
 
-app.use(cors());
+// middleware
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
 
-const chef = require('./data/chef.json');
-const recipe = require('./data/recipe.json');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const uri = `${process.env.MONGO_URI}`
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 })
 
-app.get('/chef', (req, res) => {
-    res.send(chef)
-});
+async function run() {
+    try {
+        const usersCollection = client.db('foodixirDB').collection('users')
+        const chefCollection = client.db('foodixirDB').collection('chef')
+        const recipeCollection = client.db('foodixirDB').collection('recipe')
+        const homecookCollection = client.db('foodixirDB').collection('homecook')
+        const favoriteCollection = client.db('foodixirDB').collection('favorite')
 
-app.get('/recipe', (req, res) => {
-    res.send(recipe)
-});
+        // Get User
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
 
+
+        // Saving User
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const query = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+            console.log(result);
+            res.send(result)
+        })
+
+
+
+
+        // CHEF
+
+
+        // Get all chef
+        app.get('/chef', async (req, res) => {
+            const result = await chefCollection.find().toArray();
+            res.send(result)
+        });
+
+
+        // Get Single Chef
+        app.get('/chef/:chef', async (req, res) => {
+            const chef = req.params.chef;
+            const query = { name: chef }
+            const result = await chefCollection.findOne(query);
+            res.send(result)
+        });
+
+
+
+        // HOMECOOK
+
+        // Get Homecook
+
+        app.get('/homecook', async (req, res) => {
+            const result = await homecookCollection.find().toArray();
+            res.send(result)
+        });
+
+
+
+        // Recipe
+
+        // Get all recipe
+
+        app.get('/recipe', async (req, res) => {
+            const result = await recipeCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        // Get a single recipe
+        app.get('/recipe/:chef', async (req, res) => {
+            const chef = req.params.chef;
+            const query = { chef: chef }
+            const result = await recipeCollection.find(query).toArray();
+            res.send(result)
+        });
+
+
+
+        // FAVORITE
+
+        // Fav get
+        app.get('/favorite/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await favoriteCollection.findOne(query)
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+        // Fav Put
+        app.put('/favorite/:email', async (req, res) => {
+            try {
+                const email = req.params.email;  // Corrected from "params.email" to "req.params.email"
+                const favorite = req.body;
+                const query = { email: email };
+                const options = { upsert: true };
+                const updateDoc = {
+                    $set: favorite,
+                    $addToSet: { favoriteIds: favorite.id }  // Use $addToSet to add unique items to the array
+                };
+                const result = await favoriteCollection.updateOne(query, updateDoc, options);
+
+                res.send(result)
+
+                // if (result.modifiedCount > 0) {
+                //     res.send({ success: true, message: 'Bookmark updated successfully.' });
+                // } else if (upsertedCount > 0) {
+                //     res.send({ success: true, message: 'Bookmark updated successfully.' })
+                // } else {
+                //     res.send({ success: false, message: 'Bookmark update failed.' });
+                // }
+            } catch (error) {
+                console.error('An error occurred:', error);
+                res.status(500).send({ success: false, message: 'Internal server error.' });
+            }
+        });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Foodixir Server is running..')
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Foodixir  is running on port ${port}`)
 })
